d3cc05298d139ab9fc0d38767bf12d0248ae7916
Mahfuz Shaikot <mahfuzshaikot472@gmail.com>
2026-04-14 23:21:40 +0600

    Add files via upload

---
+++ src/main/java/com/firs/project/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.firs.project.controller;
+
+import com.firs.project.model.User;
+import com.firs.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        try {
+            User savedUser = userService.registerUser(user);
+            return ResponseEntity.ok(savedUser);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+}
