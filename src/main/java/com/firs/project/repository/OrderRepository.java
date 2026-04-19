d655742d243cfd4633a480b5fb6e7a86e9ccd1d9
Mahfuz Shaikot <mahfuzshaikot472@gmail.com>
2026-04-19 07:43:48 +0600

    Delete src/main directory

--- src/main/java/com/firs/project/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firs.project.repository;
-
-import com.firs.project.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- src/main/java/com/firs/project/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firs.project.repository;
-
-import com.firs.project.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
